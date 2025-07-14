744dccb6f6bf0fa7e9c12904c2b39f6e5c46b48a
Dan102922 <motad8940@gmail.com>
2025-07-14 08:50:18 -0700

    Update ComboHelper.java

--- ComboHelper.java
+++ ComboHelper.java
@@ -1,1 +1,6 @@
-
+// Unpacks combo meals recursively
+public class ComboHelper {
+  public static List<MenuItem> unpackCombo(MenuItem combo) {
+    // If it is a combo, return each item in it recursively (when working on this)
+  }
+}

f0da165478f84cd56e9db05d0c994c7fe839e8be
Dan102922 <motad8940@gmail.com>
2025-07-14 16:46:28 +0000

    added arraylist and hashmap to main, will work on more later down the line

--- Main.java
+++ Main.java
@@ -1,1 +1,15 @@
+import java.util.HashMap;
 
+public class Main {
+    public static void main(String[] args) {
+        MenuItem[] menu = { // Array of menu items, complete with names, prices, and whether or not they are a combo
+            new MenuItem("Soda", 1.29, false)
+        };
+        // Create a HashMap to store menu items with their names as keys
+        HashMap<String, MenuItem> menuMap = new HashMap<>();
+        // Populate the HashMap with menu items
+        for (MenuItem item : menu) {
+            menuMap.put(item.getName(), item);
+        }
+    }
+}

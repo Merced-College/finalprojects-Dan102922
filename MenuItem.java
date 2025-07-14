11b808313e7829c26b83148ac56939cc841bda2f
Dan102922 <motad8940@gmail.com>
2025-07-14 08:41:45 -0700

    Added MenuItem class

--- MenuItem.java
+++ MenuItem.java
@@ -1,1 +1,6 @@
-
+//This is the class for the menu items, with the name of the item, the price, and whether or not it's a combo
+public class MenuItem {
+  private String name;
+  private double price;
+  private boolean isCombo;
+}
